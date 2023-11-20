deaea479c373245d0a1dcddd8583b1f14de9ae56
EsterDahri <esterdahri@gmail.com>
2023-11-20 18:44:05 +0100

    fix link to router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
-//import { router } from "./router.js"
+import { router } from "./router.js"
 import './assets/scss/app.scss'
 import App from './App.vue'
 
-createApp(App).mount('#app')
-//.use(router)
+createApp(App).use(router).mount('#app')
