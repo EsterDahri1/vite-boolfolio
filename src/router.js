16646556702ece7611cc24dae9d6e3ab2fb71bd2
EsterDahri <esterdahri@gmail.com>
2023-11-21 10:00:06 +0100

    fix routes in vue page

--- src/router.js
+++ src/router.js
@@ -1,13 +1,13 @@
 import { createWebHashHistory, createRouter } from "vue-router";
 import HomeView from "./views/HomeView.vue";
 import AboutView from "./views/AboutView.vue";
-import BlogView from "./views/BlogView.vue";
+import ProjectsView from "./views/ProjectsView.vue";
 import ContactsView from "./views/ContactsView.vue";
 
 const routes = [
     { path: '/', component: HomeView },
     { path: '/about', component: AboutView },
-    { path: '/blog', component: BlogView },
+    { path: '/Projects', component: ProjectsView },
     { path: '/contacts', component: ContactsView },
 ];
 
