e2f791210835991b9ea454b88afc068ddfaf3069
EsterDahri <esterdahri@gmail.com>
2023-11-21 09:30:03 +0100

    add something in page views

---
+++ src/views/BlogView.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'BlogView',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>Projects</h1>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/views/ContactsView.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'ContactsView',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>Contacts</h1>
+</template>
+
+<style lang="scss" scoped></style>
